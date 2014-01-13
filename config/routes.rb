aa36a9b6f614184c4da4f462fa068978cc44bd0a
Heriberto Perez <heriberto.perez@crowdint.com>
2014-01-13 10:22:58 -0600

    Add namespace for your api project

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,10 @@ PublicApi::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  namespace :api do
+      resources :news_item
+  end
+
   # Example resource route with options:
   #   resources :products do
   #     member do
