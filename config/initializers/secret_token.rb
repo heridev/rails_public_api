7e8ce8de148cd400caea3c3381aaadd2bb233115
Heriberto Perez <heriberto.perez@crowdint.com>
2014-01-13 10:21:35 -0600

    Create initial structure for project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PublicApi::Application.config.secret_key_base = 'ce1397bef138e7d2eb75ab6deeae3950665ecb29aec8fe7de5308b0e9ea3d99d23067821bab27720ae5598c06e162a285a49fca4c22b23a7ecb2f0e25b6582ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PublicApi::Application.config.session_store :cookie_store, key: '_public_api_session'
