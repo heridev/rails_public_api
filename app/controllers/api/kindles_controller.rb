1443659fd27783c87756a467dc4d1d69c43d5212
Heriberto Perez <heriberto.perez@crowdint.com>
2014-01-13 13:36:31 -0600

    Create kindles controller and add kaminari gem for pagination

---
+++ app/controllers/api/kindles_controller.rb
@@ -0,0 +1,12 @@
+class Api::KindlesController < ApplicationController
+  respond_to :json
+  PER_PAGE_RECORDS = 10
+
+  def index
+    kindles_paginated = Kindle.order('id').page(params[:page]).per(PER_PAGE_RECORDS)
+
+    respond_to do |format|
+      format.json { render :json => { kindles: kindles_paginated, current_page: params[:page].to_i, perPage: PER_PAGE_RECORDS, total_pages: kindles_paginated.num_pages }, callback: params[:callback] }
+    end
+  end
+end
