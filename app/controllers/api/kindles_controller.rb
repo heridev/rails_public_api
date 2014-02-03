f3a66c6398e2489c63078a872d1a5da45d26671a
Heriberto Perez <heriberto.perez@crowdint.com>
2014-02-03 15:18:38 -0600

    Implement CRUD in kindles controller after CORS implementation

--- app/controllers/api/kindles_controller.rb
+++ app/controllers/api/kindles_controller.rb
@@ -2,11 +2,39 @@ class Api::KindlesController < ApplicationController
   respond_to :json
   PER_PAGE_RECORDS = 9
 
+  skip_before_filter :verify_authenticity_token
+
   def index
     kindles_paginated = Kindle.order('id').page(params[:page]).per(PER_PAGE_RECORDS)
+    json_response = {
+      models: kindles_paginated,
+      current_page: params[:page].to_i,
+      perPage: PER_PAGE_RECORDS,
+      total_pages: kindles_paginated.num_pages
+    }
+    respond_with json_response
+  end
 
-    respond_to do |format|
-      format.json { render :json => { models: kindles_paginated, current_page: params[:page].to_i, perPage: PER_PAGE_RECORDS, total_pages: kindles_paginated.num_pages }, callback: params[:callback] }
-    end
+  def create
+    kindle = Kindle.create(kindle_params)
+    respond_with kindle, location: nil
+  end
+
+  def show
+    respond_with Kindle.find(params[:id])
+  end
+
+  def update
+    respond_with Kindle.update(params[:id], kindle_params)
+  end
+
+  def destroy
+    respond_with Kindle.destroy(params[:id])
+  end
+
+  private
+
+  def kindle_params
+    params.require(:kindle).permit(:inventory_id, :status)
   end
 end
