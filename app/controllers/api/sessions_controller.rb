1233f526d878a22c84d774c20f5922783178bddf
Heriberto Perez <heriberto.perez@crowdint.com>
2014-02-13 15:32:31 -0600

    Ensure google client id is set as environment variable

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,6 +4,7 @@ class Api::SessionsController < ApplicationController
   def create
     client_id = ENV['CLIENT_ID']
     access_token = params[:access_token]
+    is_google_client_present? client_id
     token_status = AuthTokenService.verify_token(access_token, client_id)
     user_info = AuthUserService.create(access_token, token_status)
 
@@ -18,6 +19,17 @@ class Api::SessionsController < ApplicationController
     access_token = params[:access_token]
     user = User.destroy_session(access_token)
     respond_with user
+  end
+
+  private
+
+  def is_google_client_present? client_id
+    if client_id.blank?
+      Rails.logger.error '====================================================='
+      Rails.logger.error 'The Google Client id is empty please do the following'
+      Rails.logger.error 'run in console export CLIENT_ID=yournumberofclientid'
+      Rails.logger.error '====================================================='
+    end
   end
 end
 
