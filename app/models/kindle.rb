ffa1fa8d174bf9a82ba760b197b03ad118c21af5
Heriberto Perez <heriberto.perez@crowdint.com>
2014-02-17 14:44:00 -0600

    Add validation rules for kindle model

--- app/models/kindle.rb
+++ app/models/kindle.rb
@@ -1,2 +1,9 @@
 class Kindle < ActiveRecord::Base
+  validates_presence_of :status,
+    message: 'Please fill the status'
+
+  validates_presence_of :inventory_id,
+    message: 'Please fill the Inventory Id'
+
+  validates_uniqueness_of :inventory_id
 end
