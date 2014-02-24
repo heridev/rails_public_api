7360d4b591e9290345af9b25779dc93ab9214f31
Heriberto Perez <heriberto.perez@crowdint.com>
2014-02-24 14:17:45 -0600

    Send notification only if there are users with expired requests

--- app/controllers/api/job_services_controller.rb
+++ app/controllers/api/job_services_controller.rb
@@ -3,8 +3,8 @@ class Api::JobServicesController < ApplicationController
   respond_to :json
 
   def check_expired_kindle_requests
-    users_with_expired_requests = User.expired_requests.pluck(:name)
-    Notifier.expired_requests(users_with_expired_requests).deliver
+    users = User.expired_requests.pluck(:name)
+    Notifier.expired_requests(users).deliver unless users.empty?
     respond_with :ok
   end
 end
